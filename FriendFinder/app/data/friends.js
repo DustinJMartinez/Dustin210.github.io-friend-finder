efb63ddff23fa22b35c0a49ddf1b950df128b914
Dustin Martinez <martinezdustin736@gmail.com>
2020-01-11 12:38:21 -0600

    final files

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,55 @@
+var friends = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+
+      {
+        "name":"Drake",
+        "photo":"https://a9p9n2x2.stackpathcdn.com/wp-content/blogs.dir/1/files/2019/11/Drake-Net-Worth.jpg",
+        "scores":[
+            4,
+            2,
+            1,
+            4,
+            4,
+            1,
+            2,
+            5,
+            3,
+            1
+          ]
+      },
+
+      {
+        "name":"Lizzo",
+        "photo":"https://cdn.abcotvs.com/dip/images/5716809_112419-cc-ap-amas-red-carpet-lizzo-img.jpg?w=800&r=16%3A9",
+        "scores":[
+            2,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            2
+          ]
+      }
+
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 8080;
 var jsonParser = bodyParser.json()
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
  
 // parse various different custom JSON types as JSON
@@ -19,7 +19,9 @@ app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 // parse an HTML body into a string
 app.use(bodyParser.text({ type: 'text/html' }))
 
+require("./FriendFinder/app/routing/apiRoutes")(app);
 require("./FriendFinder/app/routing/htmlRoutes")(app);
+
 
 app.listen(PORT, function() {
     console.log("APP listening on PORT: " + PORT);
